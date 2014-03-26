984e94b16fffdd762d510bdb4bfe76c8da382926
Brian Frank <bfrank@aclenscorp.com>
2014-03-25 23:04:54 -0400

    moving to node server

---
+++ server.js
@@ -0,0 +1,36 @@
+var app = require('express.io')(),
+    express = require("express"),
+    http = require('http'),
+    mysql = require("mysql"),
+    api = express(),
+    path = require("path"),
+    application_root = __dirname;
+
+function start() {
+    app.http().io()
+
+    app.listen(8888)
+
+    console.log("Server has started.");
+
+    
+    app.configure(function() {
+        app.use(express.cookieParser());
+        app.use(express.session({
+            secret: 'ssvshjjkliyrefdsvfngmhk3435678798874esfbfggjhmfhngwrevsdbhkulnrt3'
+        }));
+        //app.use(express.logger());
+
+        app.use(express.bodyParser());
+        app.use(express.methodOverride());
+        //perform route lookup based on url and HTTP method
+        app.use(app.router);
+        app.use(express.static(__dirname + '/app'));
+
+
+    });
+
+    
+}
+
+exports.start = start;
